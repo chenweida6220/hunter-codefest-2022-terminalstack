4c6d27207f6d852d2e762067470f42e43196f8c4
Shanie Ho <shanieho02@gmail.com>
2022-01-21 19:31:42 -0500

    second commit

---
+++ responses.js
@@ -0,0 +1,70 @@
+const prompts = [
+  ["hi", "hey", "hello"],
+  ["no", "yes"],
+  ["I would like to report a crime", "911", "0"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hi! This is a 911 chatbot. Are you injured?", "Hi! This is a 911 chatbot. Are you injured?", "Hi! This is a 911 chatbot. Are you injured?"],
+  [
+    "Ok! What can I help you with? I can provide assistance if you would like to report a crime. ", "Please text 911 if you would like me to call 911 and 0 if you don't"
+  ],
+  [
+    "Please state the details of the crime and I will pass this information along. Thank you!", "Stay right where you are, 911 is coming. Thank you for using our services", "State the nature of your injuries and I will try to provide some help."
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
